fc47c9636b08aca2ffe6c712903fa1b445f55a06
Rahul Rana <163443364+student-gehu@users.noreply.github.com>
2024-07-30 21:22:28 +0530

    Add files via upload

---
+++ Array/array4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>         // write a function to count the number of odd number in an array 
+int countOdd(int arr[],int n);
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    printf("%d",countOdd(arr,6));
+
+    return 0;
+}
+
+int countOdd(int arr[], int n){
+    int count=0;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]%2!=0){                 //Odd
+            count++;
+        }
+    }
+    return count;
+}
